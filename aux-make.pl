2d53f65d565254e4f7e8f1f44c829023ca309bcf
Satoshi <merapano@gmail.com>
2021-05-20 20:02:18 +0900

    Add novel.bib to $mybibdata

--- aux-make.pl
+++ aux-make.pl
@@ -3,7 +3,7 @@
 use Getopt::Long 'GetOptions';
 # use Getopt::Long;
 
-my $mybibdata = "/home/satoshi/anthrop/biblio/phil,/home/satoshi/anthrop/biblio/anthrop,/home/satoshi/anthrop/biblio/neighbor,/home/satoshi/anthrop/biblio/ntt,/home/satoshi/anthrop/biblio/satoshi,/home/satoshi/anthrop/biblio/sci,/home/satoshi/anthrop/biblio/asia";
+my $mybibdata = "/home/satoshi/anthrop/biblio/phil,/home/satoshi/anthrop/biblio/anthrop,/home/satoshi/anthrop/biblio/neighbor,/home/satoshi/anthrop/biblio/ntt,/home/satoshi/anthrop/biblio/satoshi,/home/satoshi/anthrop/biblio/sci,/home/satoshi/anthrop/biblio/asia,/home/satoshi/anthrop/biblio/novel";
 
 my $auxname = "bib.aux";
 
